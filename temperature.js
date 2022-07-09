99aac433c13cb46f9cb5c71418b2c4670e87ef0a
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2022-07-08 20:17:12 -0500

    Update temperature.js

--- temperature.js
+++ temperature.js
@@ -1,24 +1,20 @@
-/*
- * Programming Quiz: Converting Tempatures (2-2)
+/* Converting Tempatures
  * The Celsius-to-Fahrenheit formula:
  *
  *    F = C x 1.8 + 32
  *
  * 1. Set the fahrenheit variable to the correct value using the celsius variable and the forumla above
- * 2. Log the fahrenheit variable to the console
- *
- */
+ * 2. Log the fahrenheit variable to the console */
 
-/*
- * REQUIREMENTS
+/* REQUIREMENTS
  * 1. Your code should have a variable `celsius`
  * 2. You code should have a variable `fahrenheit`
  * 3. Your variable `celsius` should equal `12`
  * 4. Your variable `fahrenheit` should produce the output equal `53.6`
  * 5. Your variable `fahrenheit` declaration should use the `celsius` variable
  * 6. Your variable `fahrenheit` should have the correct formula
- * 7. Your code should log the `fahrenheit` variable
- */
+ * 7. Your code should log the `fahrenheit` variable */
+
 
 var celsius = 12;
 var fahrenheit = (celsius * 1.8 + 32);
