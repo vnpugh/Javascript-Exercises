0418f3cfb05c8c098d6864419d7cc70140ea6c9c
Velvet Pugh <48695329+vnpugh@users.noreply.github.com>
2022-07-08 20:25:15 -0500

    Add files via upload

---
+++ string.js
@@ -0,0 +1,10 @@
+/* String Concatenation
+ * Build a string using concatenation by combining lines.
+
+REQUIREMENTS
+ * 1. Your code should have a variable `poem`
+ * 2. Your code should use string concatenation
+ * 3. Each string should be printed on its own line */
+
+var poem = "Blowing from the south" + "\nFallen leaves stare" + "\nIn the west."
+console.log(poem);
